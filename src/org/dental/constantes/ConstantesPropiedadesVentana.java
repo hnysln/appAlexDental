f372772598036034ca66252a318a44e9b8cb06a8
Henry Salinas <quimicman2@hotmail.com>
2015-11-29 10:48:41 -0500

    Carga de primeras pantallas

---
+++ src/org/dental/constantes/ConstantesPropiedadesVentana.java
@@ -0,0 +1,17 @@
+/**
+ * ConstantesPropiedadesVentana.java
+ * 26/11/2015
+ */
+package org.dental.constantes;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author Henry Salinas A.
+ * @version: 1.0
+ */
+public final class ConstantesPropiedadesVentana {
+   
+
+}
---
+++ src/org/dental/constantes/ConstantesURLS.java
@@ -0,0 +1,16 @@
+/**
+ * ConstantesURLS.java
+ * 26/11/2015
+ */
+package org.dental.constantes;
+
+/**
+ *
+ * @author Henry Salinas A.
+ * @version: 1.0
+ */
+public final class ConstantesURLS {
+
+    public static final String RUTA_IMAGEN_FONDO_SECUNDARIO = "/org/dental/imagenes/FondoSecundario.jpg";
+
+}
